37a7cafe42a5feb5028fbdcb8a5dc250165106e9
yday <yangdayeon2@gmail.com>
2022-08-30 15:47:50 +0900

    Thread

--- src/thread06/MultiThreadExam.java
+++ src/thread06/MultiThreadExam.java
@@ -5,10 +5,11 @@ public class MultiThreadExam extends Thread {
     private int num;
 
     public MultiThreadExam(String s, Summing sum, int num) {
-        super(s);
-        this.sum = sum;
-        this.num = num;
-        System.out.println("스레드 : " + getName() + "가 시작 됨");
+        super(s);   // 상위 클래스(Thread)의 생성자를 호출. Thread(s)
+        // 스레드의 이름을 설정
+        this.sum = sum; // 현재 실행하는 스레드의 객체에서 sum 객체
+        this.num = num; // 현재 실행하는 스레드의 객체에서 num 값
+        System.out.println("스레드 : " + this.getName() + "가 시작 됨");
     }
 
     public void run() {
@@ -19,8 +20,8 @@ public class MultiThreadExam extends Thread {
     public static void main(String[] args) {
         Summing sum = new Summing();
         MultiThreadExam a = new MultiThreadExam("A", sum, 5);
-        MultiThreadExam b = new MultiThreadExam("B", sum, 5);
-        a.setPriority(8);
+        MultiThreadExam b = new MultiThreadExam("B", sum, 7);
+        a.setPriority(1);   // 우선 순위
         b.setPriority(10);
         a.start();
         b.start();
--- src/thread06/Summing.java
+++ src/thread06/Summing.java
@@ -3,15 +3,18 @@ package thread06;
 public class Summing {
     private int sum;
 
-    public synchronized void sumTo(int num) {
+    public synchronized void sumTo(int num) {   // 메소드를 동기화
+        // 동기화는 스레드 객체가 먼저 실행하는 쪽이 이 메소드를
+        // 실행 종료까지 점유하게 됨
         sum = 0;
         for(int i = 1; i <= num; i++) {
             sum += i;
+            // 현재 스레드의 이름을 출력한다.
             System.out.print("스레드 : " + Thread.currentThread().getName());
             System.out.println("의 1 ~ " + i + "까지 합은 " + sum);
 
             try {
-                Thread.sleep(500);
+                Thread.sleep(500);  // 0.5초 실행을 지연
             } catch (InterruptedException e) {}
         }
     }
