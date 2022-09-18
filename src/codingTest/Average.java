ebac7fc8c8bca4ec1e93264428aca12806f0de35
yday <yangdayeon2@gmail.com>
2022-09-18 22:16:45 +0900

    평균 구하기

---
+++ src/codingTest/Average.java
@@ -0,0 +1,17 @@
+package codingTest;
+
+public class Average {
+
+    public double solution(int[] arr) {
+        double answer = 0;
+        double result = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
+
+        answer = result / arr.length;
+        return answer;
+    }
+
+}
