3aecddda9ccb9518d098e861c57035badd6233e3
yday <yangdayeon2@gmail.com>
2022-09-21 11:00:07 +0900

    FlowLayout

---
+++ src/awtgui03/FlowLayoutEx.java
@@ -0,0 +1,26 @@
+package awtgui03;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutEx extends JFrame {
+    public FlowLayoutEx() {
+        setTitle("FlowLayout Sample");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+
+        c.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
+        c.add(new JButton("add"));
+        c.add(new JButton("sub"));
+        c.add(new JButton("muliply"));
+        c.add(new JButton("divide"));
+        c.add(new JButton("Calculate"));
+
+        setSize(300, 200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FlowLayoutEx();
+    }
+}
